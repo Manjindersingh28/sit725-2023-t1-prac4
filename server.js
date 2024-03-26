0a0a54e65300abb1433b8c6fa9566a3d008f65f7
Manjinder Singh <mani28au@gmail.com>
2024-03-26 12:38:11 +1100

    SIT725 prac 3 initial changes

---
+++ server.js
@@ -0,0 +1,17 @@
+/*var express = require("express")
+var app = express()
+var port = process.env.PORT || 3000;
+
+app.listen(port,()=> {
+console.log("App listening to: "+port)
+})*/
+
+
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000; 
+
+app.listen(port, () => {
+    console.log("App listening to port: " + port);
+});
+ 
