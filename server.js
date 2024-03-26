ed3c824ec4dd1d7b1b96ee5f2e4b539a94c3796b
Manjinder Singh <mani28au@gmail.com>
2024-03-26 21:22:45 +1100

    Added all project files

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require("express");
 var app = express();
-
-app.use(express.static(__dirname+'/public'))
-app.use(express.json()); app.use(express.urlencoded({
-    extended:false }));
-
-
 var port = process.env.PORT || 3000; 
 
+console.log('dfdfdf', __dirname);
+app.use(express.static(__dirname+'/public'));
+app.use(express.json());
+app.use(
+express.urlencoded({
+    extended: false,
+}));
 app.listen(port, () => {
     console.log("App listening to port: " + port);
 });
